fe9989d734165541089c50a6649e619233b0aea1
eric <eric7252000@gmail.com>
2019-12-21 14:14:22 -0500

    Fixed imports and fields in history

--- src/data/history.py
+++ src/data/history.py
@@ -1,20 +1,22 @@
 import pymongo
 from datetime import datetime
-from typing import Dict, List, Any, field
+from typing import Dict, List, Any
 from collections import namedtuple
 from bson import ObjectId
+from dataclasses import dataclass, field
 
 from bson.codec_options import TypeCodec
 
 Version = namedtuple('Version', 'version_num creation_date title author body tags')
+
 @dataclass
 class Diff:
-    version_num: int
+    version_num: int = 0
     timestamp: datetime = field(default_factory=lambda: datetime.now())
-    author: str
-    tags_removed: List[str]
-    tags_added: List[str] 
-    body_update: Dict[str, Any]
+    author: str = ''
+    tags_removed: List[str] = field(default_factory=lambda: [])
+    tags_added: List[str] = field(default_factory=lambda: [])
+    body_update: Dict[str, Any] = field(default_factory=lambda: {})
     _id: ObjectId = field(default_factory=lambda:ObjectId())
 
 
