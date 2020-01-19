4168ef0be37fe7428a6fba4b63177e9cdfd890ec
eric <eric7252000@gmail.com>
2020-01-18 21:27:12 -0500

    Added pokemon information

---
+++ generate.py
@@ -0,0 +1,42 @@
+import json
+
+from dataclasses import is_dataclass, asdict
+from enum import Enum
+from pathlib import Path
+from typing import Dict, Any
+
+
+from src.scraper.scrape import complete_scrape
+
+from jinja2 import Template, FileSystemLoader, Environment
+
+
+class JsonEncoderCodec(json.JSONEncoder):
+    transforms: Dict[Any, Any] = {}
+
+    def default(self, z):
+        if issubclass(type(z), Enum):
+            return z.name
+
+        if is_dataclass(z):
+            return asdict(z)
+
+        if type(z) in JsonEncoderCodec.transforms:
+            return JsonEncoderCodec.transforms[type(z)](z)
+        else:
+            return super().default(z)
+
+pokedex: Dict = complete_scrape()
+
+pokedex = {k: json.dumps(v, cls=JsonEncoderCodec, indent=2) for k, v in pokedex.items()}
+
+current_dir = Path(__file__).parent
+template_dir = current_dir/'templates'
+
+template_loader = FileSystemLoader(searchpath=str(template_dir))
+template_env = Environment(loader=template_loader, autoescape=True)
+
+out = template_env.get_template('index.html').render(pokedex=pokedex)
+
+with open(str(current_dir/'index.html'), 'w') as f:
+    print(out, file=f)
